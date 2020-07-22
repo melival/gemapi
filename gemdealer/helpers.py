da4a4238497effe8624c5b2703d3e7dcde75d883
melival <melival84@gmail.com>
2020-07-23 04:48:52 +1100

    Added exceptions on parse. Added env settings.

--- gemdealer/helpers.py
+++ gemdealer/helpers.py
@@ -1,17 +1,24 @@
 from django.db.models import Sum
 
 
-def save_data(Model, data):
-    Model(**data).save()
-    # print(data)
-
-
-def upload_from_csv(data, Model):
+def csv_reader(data):
+    """Парсинг csv файла.
+    Стандартный csv.reader с первого раза не заработал - написал свой.
+    """
     t = data.split('\r\n')
-    header, table = t[0], t[1:]
-    print(header)
+    if len(t) < 2: # Если строчек мало получилось
+        raise RuntimeError("No data in csv source.")
 
+    header, table = t[0], t[1:]
     rows = [row.split(',') for row in table if row]
+    return header, rows
+
+
+def save_row(Model, data):
+    Model(**data).save()
+
+
+def save_data(rows, Model):
     for customer, item, total, quantity, date in rows:
         data = {
             'customer': customer,
@@ -20,9 +27,18 @@ def upload_from_csv(data, Model):
             'quantity': int(quantity),
             'date': date,
         }
-        save_data(Model, data)
+        save_row(Model, data)
 
-    return {'status': "Ok"}
+
+def upload_from_csv(data, Model):
+    try:
+        _, rows = csv_reader(data)
+        Model.objects.all().delete() # Про требование №2.
+        save_data(rows, Model)
+    except Exception as e:
+        return {'Status': "Error", 'Desc': "Can't render csv data."}
+
+    return {'Status': "Ok"}
 
 
 def get_customers(query):
@@ -47,16 +63,19 @@ def get_popular_gems(query):
     return set([gem for gem in gems_counts.keys() if gems_counts[gem] > 1])
 
 
-def get_data(Model=None):
+def get_data(Model):
     # Выбираем любимых покупателей
-    result = get_customers( (Model.objects.values('customer').
-                                           annotate(summary=Sum('total')).
-                                           order_by('-summary')
-                            )[:5] )
+    result = get_customers(
+        ( Model.objects.values('customer').
+                        annotate(summary=Sum('total')).
+                        order_by('-summary')
+        )[:5] )
 
     # Заполняем списки купленных камней
     for uname in result.keys():
-        q = Model.objects.filter(customer=uname).values_list('item', flat=True)
+        q = ( Model.objects.filter(customer=uname).
+                            values_list('item', flat=True).
+                            distinct() )
         result.get(uname).get('gems').update(q)
 
     # Выбираем популярные камни
